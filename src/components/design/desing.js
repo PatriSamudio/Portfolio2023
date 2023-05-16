ee72537b61169be4e4d4a4f34d63dc1c6958890a
Patricia Samudio <patty_samudio@hotmail.com>
2023-05-16 11:47:35 +0200

    add more funtion

--- src/components/design/desing.js
+++ src/components/design/desing.js
@@ -1,18 +1,6 @@
-const grande = document.querySelector(".grande");
-const punto = document.querySelectorAll(".punto");
+const toggleBtn = document.querySelector(".toggle-btn");
+const listContainer = document.querySelector(".list-container");
 
-punto.forEach((cadaPunto, i) => {
-  punto[i].addEventListener("click", () => {
-    const position = i;
-
-    const operation = position * -20;
-
-    grande.style.transform = `translateX(${operation}%)`;
-
-    punto.forEach((cadaPunto, i) => {
-      punto[i].classList.remove("activo");
-    });
-
-    punto[i].classList.add("activo");
-  });
+toggleBtn.addEventListener("click", () => {
+  listContainer.classList.toggle("flex");
 });
