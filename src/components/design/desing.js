7dc25e60c405451405f7305531b029322d599ad7
Patricia Samudio <patty_samudio@hotmail.com>
2023-05-16 19:06:57 +0200

    add design, fotografia, frontend and design

--- src/components/design/desing.js
+++ src/components/design/desing.js
@@ -1,6 +1,26 @@
+const grande = document.querySelector(".grande");
+const punto = document.querySelectorAll(".punto");
+
+punto.forEach((cadaPunto, i) => {
+  punto[i].addEventListener("click", () => {
+    const position = i;
+
+    const operation = position * -22;
+
+    grande.style.transform = `translateX(${operation}%)`;
+
+    punto.forEach((cadaPunto, i) => {
+      punto[i].classList.remove("activo");
+    });
+
+    punto[i].classList.add("activo");
+  });
+});
+
 const toggleBtn = document.querySelector(".toggle-btn");
 const listContainer = document.querySelector(".list-container");
 
 toggleBtn.addEventListener("click", () => {
+  // when clicked show the hidden items
   listContainer.classList.toggle("flex");
 });
