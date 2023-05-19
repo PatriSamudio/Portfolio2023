1275f0d753a1824afad6285a84981d59edbc9553
Patricia Samudio <patty_samudio@hotmail.com>
2023-05-19 19:34:38 +0200

    fixed cv

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
+// const section = document.querySelector(".section");
+
+// window.addEventListener("scroll", function () {
+//   const value = window.scrollY;
+//   section.style.clipPath = "circle(" + value + "px at center)";
+// });
+
 const section = document.querySelector(".section");
 
-window.addEventListener("scroll", function () {
+function esDispositivoMovil() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+function aplicarEfectoScroll() {
   const value = window.scrollY;
   section.style.clipPath = "circle(" + value + "px at center)";
-});
+}
+
+// Verificar si se trata de un dispositivo móvil antes de agregar el evento de desplazamiento
+if (!esDispositivoMovil()) {
+  window.addEventListener("scroll", aplicarEfectoScroll);
+}
